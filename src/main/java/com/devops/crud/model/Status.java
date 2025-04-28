7c6d50dd28477b079e3e2687861b34f72b67296a
nadheem <nadimbuddy66@gmail.com>
2025-04-28 23:08:47 +0530

    Add new sb crud project

---
+++ src/main/java/com/devops/crud/model/Status.java
@@ -0,0 +1,31 @@
+package com.devops.crud.model;
+
+public class Status {
+
+	public String message;
+
+	public String code;
+
+	public Status(String message, String code) {
+		super();
+		this.message = message;
+		this.code = code;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	public String getCode() {
+		return code;
+	}
+
+	public void setCode(String code) {
+		this.code = code;
+	}
+
+}
